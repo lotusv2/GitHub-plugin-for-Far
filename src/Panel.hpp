f37d5286a3bd0ea945a6d6c537b07c9772e01e64
lotusv2 <glotus@gmail.com>
2026-09-09 12:12:01 +0300

    Add Far panel interface

---
+++ src/Panel.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <plugin.hpp>
+#include "GitHubClient.hpp"
+
+class FarGitHubPanel
+{
+public:
+    FarGitHubPanel();
+
+    intptr_t GetFindData(PluginPanelItem** items, size_t* count, OPERATION_MODES mode);
+    void FreeFindData(PluginPanelItem* items, size_t count);
+    void GetOpenPanelInfo(OpenPanelInfo* info);
+    intptr_t SetDirectory(const wchar_t* directory, OPERATION_MODES mode);
+    intptr_t ProcessHostFile(PluginPanelItem* items, size_t count, OPERATION_MODES mode);
+    intptr_t MakeDirectory(const wchar_t* name, OPERATION_MODES mode);
+
+private:
+    std::wstring Token;
+    std::wstring Repository;
+    std::wstring CurrentPath;
+    std::vector<GitHubEntry> Entries;
+    std::wstring Error;
+
+    bool Reload();
+    std::wstring FullPath(const std::wstring& name) const;
+    bool EditFile(const std::wstring& path);
+};
