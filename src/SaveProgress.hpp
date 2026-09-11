07fc1e1710fc119961267c0ab7644d0789b8314f
lotusv2 <glotus@gmail.com>
2026-09-11 09:22:51 +0300

    Prepare Far editor save session for F2

--- src/SaveProgress.hpp
+++ src/SaveProgress.hpp
@@ -1,5 +1,15 @@
 #pragma once
 
 #include <functional>
+#include <string>
 
 bool RunGitHubProgress(const wchar_t* text, const std::function<bool()>& operation);
+
+void BeginGitHubEditorSession(const std::wstring& tempFile,
+                              const std::wstring& remotePath,
+                              const std::wstring& remoteSha,
+                              const std::wstring& token,
+                              const std::wstring& repository,
+                              const std::wstring& branch);
+
+void EndGitHubEditorSession();
