6de60e5261a1ce7ba571a6c2a965fb900d1e3736
lotusv2 <glotus@gmail.com>
2026-09-11 09:00:36 +0300

    Fix Far-native save progress handling

--- src/SaveProgress.hpp
+++ src/SaveProgress.hpp
@@ -1,8 +1,5 @@
 #pragma once
 
-#include <windows.h>
-
 #include <functional>
 
 bool RunGitHubProgress(const wchar_t* text, const std::function<bool()>& operation);
-void ShowGitHubProgressTimed(const wchar_t* text, DWORD timeoutMs);
